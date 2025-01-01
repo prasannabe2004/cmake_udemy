268dc5d14e3b4339a258514e1305d57085ca6bb6
prasannabe2004 <prasannabe2004@gmail.com>
2025-01-01 21:50:34 +0530

    Project 8 renamed

--- 2_CMake/08_FetchContent/src/my_lib/my_lib.cpp
+++ 2_CMake/08_FetchContent/src/my_lib/my_lib.cpp
@@ -6,7 +6,7 @@
 
 void print_hello_world()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Project 8 checkout git renamed!\n";
 
     std::cout << "JSON Lib Version:" << NLOHMANN_JSON_VERSION_MAJOR << "." << NLOHMANN_JSON_VERSION_MINOR << "."
               << NLOHMANN_JSON_VERSION_PATCH << "\n";
